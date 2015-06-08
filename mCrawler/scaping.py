6d4cfd79d408a285db429806527b931d1eb7b4a6
allan <dev@la-becanerie.com>
2015-06-08 13:22:46 +0200

    dev-96

--- mCrawler/scaping.py
+++
@@ -1,9 +0,0 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
-__author__ = "allan"
-__date__ = "$8 juin 2015 13:15:08$"
-
-if __name__ == "__main__":
-    print "Hello World"
