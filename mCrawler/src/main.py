232027f6064af4d74f5e510da2ac8279e74c081a
allan <allanjegu@gmail.com>
2015-07-01 20:22:08 +0200

    encapsulation des erreurs

--- mCrawler/src/main.py
+++ mCrawler/src/main.py
@@ -17,109 +17,116 @@ opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
 err = 0
 while(True):
-    nid = Imdb.getLastImdbID()
-    sid = str(nid)
-        
-    movieUrl = "http://www.imdb.com/title/tt"  + sid.zfill(7)
     log = open('main.log', 'a')
-    log.write(str(datetime.datetime.now()) + " " + movieUrl + "\n")
-    
+    nid = False
     try:
-        request = opener.open(movieUrl)        
-    except urllib2.HTTPError:
-        log.write("erreur de connection" + "\n")
-        err +=1
-        if(err > 10000):
-            log.write("Arret du crawl" + "\n")
-            break
-    except urllib2.URLError:
-        log.write("erreur movie url " + movieUrl + "\n")
-    else:
-        movie = False
+        nid = Imdb.getLastImdbID()
+    except:
+        log.write("erreur du crawler - recuperation du imbb_id\n")
+
+    if (nid != False):
+        sid = str(nid)
+        
+        movieUrl = "http://www.imdb.com/title/tt"  + sid.zfill(7)
+
+        log.write(str(datetime.datetime.now()) + " " + movieUrl + "\n")
+
         try:
-            movie = Scraping.getMovie(request.read())
-        except:
-            log.write("erreur du crawler " + movieUrl + "\n")
+            request = opener.open(movieUrl)
+        except urllib2.HTTPError:
+            log.write("erreur de connection" + "\n")
+            err +=1
+            if(err > 10000):
+                log.write("Arret du crawl" + "\n")
+                break
+        except urllib2.URLError:
+            log.write("erreur movie url " + movieUrl + "\n")
+        else:
+            movie = False
+            try:
+                movie = Scraping.getMovie(request.read())
+            except:
+                log.write("erreur du crawler " + movieUrl + "\n")
 
-        if (movie != False):
-            if(movie['type'] == ''):
+            if (movie != False):
+                if(movie['type'] == ''):
 
-                movie['imdb_id'] = nid
-                id_movie = False
-                try:
-                    id_movie = Movie.save(movie)
-                except:
-                    log.write("can't save movie" + movieUrl + "\n")
-
-                if (id_movie != False):
-                    for actor in movie['actors']:
-                        id_actor = False
-                        try:
-                            id_actor = Actor.find(actor.strip())
-                        except:
-                            log.write("can't find actor" + movieUrl + "\n")
-                        if(id_actor == False):
-                            try:
-                                id_actor = Actor.save(actor.strip())
-                            except:
-                                log.write("can't save actor" + movieUrl + "\n")
-                        if (id_actor == False):
-                            try:
-                                Movie.addActor(id_movie, id_actor)
-                            except:
-                                log.write("can't associate actor-movie" + movieUrl + "\n")
-
-                    for genre in movie['genres']:
-                        id_genre = False
-                        try:
-                            id_genre = Genre.find(genre.strip())
-                        except:
-                            log.write("can't find genre" + movieUrl + "\n")
-                        if(id_genre == False):
-                            try:
-                                id_genre = Genre.save(genre.strip())
-                            except:
-                                log.write("can't save genre" + movieUrl + "\n")
-                            if(id_genre != False):
-                                try:
-                                    Movie.addGenre(id_movie, id_genre)
-                                except:
-                                    log.write("can't associate genre-movie" + movieUrl + "\n")
-
-                    time.sleep(1)
-                    directorsUrl = "http://www.imdb.com/title/tt{0}/fullcredits?ref_=tt_ov_dr#directors".format(sid)
+                    movie['imdb_id'] = nid
+                    id_movie = False
                     try:
-                        request = opener.open(directorsUrl)
-                    except urllib2.HTTPError:
-                        print "can't to open directory url " + directorsUrl
-                    except urllib2.URLError:
-                        print "error directory url " + directorsUrl
+                        id_movie = Movie.save(movie)
                     except:
-                        log.write("can't open director" + directorsUrl + "\n")
-                    else:
-                        try:
-                            directors = Scraping.getDirectors(request.read())
-                        except:
-                            log.write("can't scaping" + directorsUrl + "\n")
-                        for director in directors:
-                            id_director = False
+                        log.write("can't save movie" + movieUrl + "\n")
+
+                    if (id_movie != False):
+                        for actor in movie['actors']:
+                            id_actor = False
                             try:
-                                id_director = Director.find(director.strip())
+                                id_actor = Actor.find(actor.strip())
                             except:
-                                log.write("can't find director" + directorsUrl + "\n")
-                            if(id_director == False):
+                                log.write("can't find actor" + movieUrl + "\n")
+                            if(id_actor == False):
                                 try:
-                                    id_director = Director.save(director.strip())
+                                    id_actor = Actor.save(actor.strip())
                                 except:
-                                    log.write("can't save" + directorsUrl + "\n")
-
-                            if(id_director != False):
+                                    log.write("can't save actor" + movieUrl + "\n")
+                            if (id_actor == False):
                                 try:
-                                    Movie.addDirector(id_movie, id_director)
+                                    Movie.addActor(id_movie, id_actor)
                                 except:
-                                    log.write("can't associate director-movie" + directorsUrl + "\n")
+                                    log.write("can't associate actor-movie" + movieUrl + "\n")
 
-    #rand = random.randrange(1,1)
+                        for genre in movie['genres']:
+                            id_genre = False
+                            try:
+                                id_genre = Genre.find(genre.strip())
+                            except:
+                                log.write("can't find genre" + movieUrl + "\n")
+                            if(id_genre == False):
+                                try:
+                                    id_genre = Genre.save(genre.strip())
+                                except:
+                                    log.write("can't save genre" + movieUrl + "\n")
+                                if(id_genre != False):
+                                    try:
+                                        Movie.addGenre(id_movie, id_genre)
+                                    except:
+                                        log.write("can't associate genre-movie" + movieUrl + "\n")
+
+                        time.sleep(1)
+                        directorsUrl = "http://www.imdb.com/title/tt{0}/fullcredits?ref_=tt_ov_dr#directors".format(sid)
+                        try:
+                            request = opener.open(directorsUrl)
+                        except urllib2.HTTPError:
+                            print "can't to open directory url " + directorsUrl
+                        except urllib2.URLError:
+                            print "error directory url " + directorsUrl
+                        except:
+                            log.write("can't open director" + directorsUrl + "\n")
+                        else:
+                            try:
+                                directors = Scraping.getDirectors(request.read())
+                            except:
+                                log.write("can't scaping" + directorsUrl + "\n")
+                            for director in directors:
+                                id_director = False
+                                try:
+                                    id_director = Director.find(director.strip())
+                                except:
+                                    log.write("can't find director" + directorsUrl + "\n")
+                                if(id_director == False):
+                                    try:
+                                        id_director = Director.save(director.strip())
+                                    except:
+                                        log.write("can't save" + directorsUrl + "\n")
+
+                                if(id_director != False):
+                                    try:
+                                        Movie.addDirector(id_movie, id_director)
+                                    except:
+                                        log.write("can't associate director-movie" + directorsUrl + "\n")
+
+        #rand = random.randrange(1,1)
     time.sleep(1)
     #nid -= 1
     log.close()
